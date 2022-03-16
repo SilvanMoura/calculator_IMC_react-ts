ef2dd28defb6fcc9e8ea1418eade6dcd15012720
Silvan Moura <89323396+SilvanMoura@users.noreply.github.com>
2022-03-16 17:37:20 -0300

    Finalizando a base da estrutura do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,26 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import styles from './App.module.css';
+import poweredImage from './assets/poweredImage.png'
 
-function App() {
-  const [count, setCount] = useState(0)
+let App = ()=> {
+  return(
+    <div className={styles.main}>
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+      <header>
+        <div className={styles.headerContainer}>
+          <img src={poweredImage} alt="" width={150}/>
+        </div>
       </header>
-    </div>
-  )
-}
 
-export default App
+      <div className={styles.container}>
+        <div className={styles.leftSide}>
+
+        </div>
+        <div className={styles.rightSide}>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
